be92818d1be8e3888f6e5fc846dcc89d3c4c2079
junaidNadeem <junaid.pk48@gmail.com>
2024-03-08 16:20:45 +0500

    first commit

---
+++ TypeScript/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let mansur = {
+    name: "mansur",
+    age: 10,
+    retire: (date) => { return date; }
+};
---
+++ TypeScript/src/index.ts
@@ -0,0 +1,61 @@
+//rray
+
+// let number : [number, string] = [1, "mansur"]
+
+//enum
+
+// const enum sizes {
+//     s=3,
+//     m,
+//     l
+
+// }
+
+
+// let mySize : sizes = sizes.l
+
+// console.log(mySize)
+
+// objects
+// type Employee = {
+//     readonly name: string,
+//     age: number,
+//     retire: (date: Date) => Date
+// }
+
+// let mansur: Employee = {
+//     name: "mansur",
+//     age: 10,
+//     retire : (date: Date)=> {return date}
+// }
+
+// Intersection of types
+
+// type Human = {
+//     isHuman: boolean
+// }
+
+// type Student = {
+//     isStudent: boolean
+// }
+
+// let mansur: Human & Student = {
+//     isHuman: true,
+//     isStudent: true,
+// }
+
+
+//iteral types
+
+// type Quantity = 50 | 100
+
+// let QuantitOfRice: Quantity
+
+// QuantitOfRice = 100
+
+
+//optioal chaining
+
+
+//obj?.something
+//arr?.[0]
